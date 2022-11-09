9e9187cfdf5f2126b6a54fb61c26fa7e74c63455
German Ramirez <61391387+Gerguzon@users.noreply.github.com>
2022-11-09 10:11:37 -0800

    Agregue el menu hamburguesa y el carrito de compra
    Agregue los componentes de el menu mobile con el menu hamburguesa y el carrito, que se quita al dar click en otro menu o si se pasa a mobile o desktop

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCart = document.querySelector('.navbar-shopping-cart');
+const productsCart = document.querySelector('.products-cart');
+
+
 
 menuEmail.addEventListener('click', toggleMenu);
+burgerMenu.addEventListener('click', toggleBurgerMenu);
+menuCart.addEventListener('click', toggleCartAside);
 
 function toggleMenu() {
      desktopMenu.classList.toggle('inactive');
+     productsCart.classList.add('inactive');
+}
+function toggleBurgerMenu(){
+     mobileMenu.classList.toggle('inactive');
+     productsCart.classList.add('inactive');
+     
+}
+
+function toggleCartAside(){
+     productsCart.classList.toggle('inactive');
+     mobileMenu.classList.add('inactive');
+     desktopMenu.classList.add('inactive');
+     
 }
